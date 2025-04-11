313a2bf2b44d1ff996a89eabf2b517d9d3a2227f
sonyoona <sonyoo0526@naver.com>
2025-04-11 09:29:47 +0900

    first commit

---
+++ src/main/java/com/yoona/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.yoona.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class MainController {
+	@GetMapping
+	public String intro(Model model) {
+		model.addAttribute("greeting", "Hello, Jenkins!");
+		return "intro";
+	}
+}
---
+++ src/test/java/com/yoona/JenkinscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
