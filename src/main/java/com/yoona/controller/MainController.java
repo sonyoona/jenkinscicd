bfc8dc21e557c1f7fc47861a71e1311f1346e617
sonyoona <sonyoo0526@naver.com>
2025-04-11 16:20:01 +0900

    commit

--- src/main/java/com/yoona/controller/MainController.java
+++ src/main/java/com/yoona/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@GetMapping
 	public String intro(Model model) {
-		model.addAttribute("greeting", "안녕하세요!!!");
+		model.addAttribute("greeting", "jenkins를 이용한 CI/CD!!!");
 		return "intro";
 	}
 }
